91aba7001067121fd951e229bc7d9c282dcc8517
Nick Maltbie <nick.dmalt@gmail.com>
2023-02-18 15:04:28 -0800

    Setup basic hitbox concept for project

---
+++ Assets/Scripts/Interactive/Hitbox/HumanoidHitbox.cs
@@ -0,0 +1,49 @@
+// Copyright (C) 2023 Nicholas Maltbie
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+// associated documentation files (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge, publish, distribute,
+// sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+// BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using UnityEngine;
+
+namespace nickmaltbie.Treachery.Interactive.Hitbox
+{
+    public enum HumanoidHitboxType
+    {
+        Unassigned,
+        Head,
+        Torso,
+        Arm,
+        Leg,
+        Foot,
+        Hand
+    }
+
+    [RequireComponent(typeof(Collider))]
+    public class HumanoidHitbox : MonoBehaviour, IHitbox
+    {
+        [SerializeField]
+        public HumanoidHitboxType hitboxType;
+
+        public Collider Collider => GetComponent<Collider>();
+        public bool IsCritical => hitboxType == HumanoidHitboxType.Head;
+
+        public void Awake()
+        {
+            gameObject.layer = IHitbox.HitboxLayer;
+            Collider.isTrigger = true;
+        }
+    }
+}
